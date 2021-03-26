b5ce89bfbaff350435bddaf7668d4a9950aea358
Ahmet Harun Tokyer <ahmetharuntokyer@gmail.com>
2021-03-26 16:38:25 +0300

    CollectObject added

---
+++ Assets/CollectObject/Scripts/CollectObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectObject : MonoBehaviour
+{
+    public AudioSource collectSound;
+
+    void OnTriggerEnter(Collider other)
+    {   
+        Debug.Log("çarptı");
+        collectSound.Play();
+        CollectingScore.score +=1;
+        Destroy(gameObject);
+        
+    }
+}
---
+++ Assets/CollectObject/Scripts/CollectingScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectingScore : MonoBehaviour
+{
+    public GameObject collectScore;
+    public static int score;
+    
+    void Update()
+    {
+        collectScore.GetComponent<Text>().text = "OBJECT: " + score;
+    }
+}
