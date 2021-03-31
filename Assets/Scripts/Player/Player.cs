9e838f2a0bdde2f9458ecd28f74af2a3143ca27c
Doğuhan <58668943+12ByteCivilEngineer@users.noreply.github.com>
2021-03-31 22:03:50 +0300

    Revert "merge1"
    This reverts commit 82fd6b05c657e5d3416f60094684b5b16d1943c2, reversing
    changes made to 6d5569ee26e6fbdb2c269cb8df224a787f447eb7.

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -3,15 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-<<<<<<< HEAD
 public enum MoveControl
 {
     MOVE,
     WAIT
 }
-=======
-
->>>>>>> Dogukan
 public class Player: MonoBehaviour
 {
 
