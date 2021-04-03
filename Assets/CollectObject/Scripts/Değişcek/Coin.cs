18910d590c13860f7f4ab7f7143ad04973f8820d
Oğuz SEKMEN <78972141+Oguzsekmen@users.noreply.github.com>
2021-04-03 17:57:43 +0300

    Revert "T"
    This reverts commit 50f58506b8cad914aadd9b0206bc2b9e8d7cb1b9.

---
+++ Assets/CollectObject/Scripts/Değişcek/Coin.cs
@@ -0,0 +1,11 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class Coin : ICollectible
+//{
+//    public bool OnCollect(PlayerController playerController)
+//    {
+//        return true;
+//    }
+//}
