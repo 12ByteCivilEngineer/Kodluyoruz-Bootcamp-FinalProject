c7b1b064ddd0a101e893c4a9e032f776e637588a
Doğuhan <58668943+12ByteCivilEngineer@users.noreply.github.com>
2021-04-03 22:20:43 +0300

    asd

--- Assets/Scripts/PlayerAnimations.cs
+++ Assets/Scripts/PlayerAnimations.cs
@@ -6,27 +6,33 @@ public class PlayerAnimations : MonoBehaviour
 {
     public Rigidbody rb;
     private Animator anim;
-    
+    Rigidbody[] bodies;
+
+    private void Awake()
+    {
+        bodies = GetComponentsInChildren<Rigidbody>();
+    }
+
     private void Start()
     {
         anim = GetComponent<Animator>();
-        
-
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Block"))
         {
+            anim.enabled = true;
             HandCollisionHandler[] handCollisionHandlers = FindObjectsOfType<HandCollisionHandler>();
             anim.SetBool("isColliding", true);
             foreach (HandCollisionHandler element in handCollisionHandlers)
             {
                 element.MinDistance = Mathf.Infinity;
             }
+            foreach (Rigidbody element in bodies)
+            {
+                element.isKinematic = true;
+            }
         }
-
-       
     }
-    
 }
