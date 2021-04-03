27d9b98b6bed5973c093128cb7862e3656ebf35d
Oğuz SEKMEN <78972141+Oguzsekmen@users.noreply.github.com>
2021-04-03 17:58:12 +0300

    Revert "T"
    This reverts commit 50f58506b8cad914aadd9b0206bc2b9e8d7cb1b9.

---
+++ Assets/Scripts/Player/Finish.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    public GameObject winnerScreen;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Finish"))
+        {
+            Time.timeScale = 0f;
+            winnerScreen.SetActive(true);
+            Debug.Log("Değdim");
+            TimeControl.instance.GameOver();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/UISceneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISceneManager : MonoBehaviour
+{
+    public GameObject uiScene;
+    public GameObject inGameScene;
+
+    private void Start()
+    {
+        inGameScene.SetActive(false);
+    }
+    public void StartButtonClick()
+    {
+        //uiScene.SetActive(false);
+        inGameScene.SetActive(true);
+    }
+}
