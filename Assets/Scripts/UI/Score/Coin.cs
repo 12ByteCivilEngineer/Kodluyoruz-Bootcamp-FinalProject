d11a2be1b5b110d9c74f1ccedd996e73c2359d61
Doğuhan <58668943+12ByteCivilEngineer@users.noreply.github.com>
2021-04-04 19:15:17 +0300

    asdfg

--- Assets/Scripts/UI/Score/Coin.cs
+++ Assets/Scripts/UI/Score/Coin.cs
@@ -2,31 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class Coin : MonoBehaviour
 {
+    Camera mainCam;
     public static int numberOfCoins;
     public Text coinText;
     public Text shopCoinText;
     public Text gameOverText;
-    public int coinPoint;  
-
+    public int coinPoint;
+    bool coinCollected = false;
+    [SerializeField]
+    float rotSpeed = 100f;
+    Transform target;
+    bool dummy = false;
+    float timer = 0f;
+    [SerializeField] Vector3 scaleRatio = new Vector3(0.01f, 0.01f, 0.01f);
+    [SerializeField] Vector3 coinPosIncrement = new Vector3(8f, 14f, 0f);
+    Vector3 basePos;
+    private void Start()
+    {
+        mainCam = Camera.main;
+    }
     private void Update()
     {
         coinText.text = PlayerPrefs.GetInt("NumberOfCoins").ToString();
         shopCoinText.text = PlayerPrefs.GetInt("NumberOfCoins").ToString();
         gameOverText.text = PlayerPrefs.GetInt("NumberOfCoins").ToString();
+        if (dummy)
+        {
+            timer += Time.deltaTime;
+            transform.position = Vector3.Lerp(basePos, new Vector3 (mainCam.transform.position.x, mainCam.transform.position.y,basePos.z) + coinPosIncrement, timer / 3f);
+            transform.localScale = Vector3.Lerp(new Vector3(1f,1f,1f), scaleRatio, timer / 3f);
+            transform.Rotate(0f, 0f, rotSpeed * Time.deltaTime);
+        }
     }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !coinCollected)
         {
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(2).gameObject.SetActive(true);
+            basePos = transform.position;
+            if (target== null) { target = GameObject.FindGameObjectWithTag("CoinTarget").transform; }
+            coinCollected = true;
+            dummy = true;
             numberOfCoins = PlayerPrefs.GetInt("NumberOfCoins") + coinPoint;
             PlayerPrefs.SetInt("NumberOfCoins", numberOfCoins);
             coinText.text = PlayerPrefs.GetInt("NumberOfCoins").ToString();
             shopCoinText.text = PlayerPrefs.GetInt("NumberOfCoins").ToString();
             gameOverText.text = PlayerPrefs.GetInt("NumberOfCoins").ToString();
-            Destroy(gameObject);
+            Destroy(gameObject,3f);
         }
     }
 }
