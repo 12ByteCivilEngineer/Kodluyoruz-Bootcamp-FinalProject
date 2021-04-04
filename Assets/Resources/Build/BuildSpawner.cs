de4778f58584e53b950920de271cd3e20ecdb8ea
Ahmet Harun Tokyer <ahmetharuntokyer@gmail.com>
2021-04-04 12:38:50 +0300

    Bina spawner added

---
+++ Assets/Resources/Build/BuildSpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildSpawner : MonoBehaviour
+{
+    Transform buildContainer;
+    
+    public static GameObject[] buildObjects;
+
+    public static int numSpawned = 0;
+    [SerializeField]
+    int buildnumber;
+
+    void Start()
+    {
+        buildObjects = Resources.LoadAll<GameObject>("Prefabs");
+    }
+    
+
+    public BuildSpawner(Transform buildContainer)
+    {
+        this.buildContainer = buildContainer;
+    }
+
+    public void Spawn()
+    {
+        int whichItem = Random.Range (0, 4);
+        
+        GameObject build = Instantiate (buildObjects[whichItem]) as GameObject;
+
+        
+        if(numSpawned == 0)
+        {
+            build.transform.position = new Vector3(0f,0f,0f);
+
+        }
+        else
+        {
+            build.transform.position = new Vector3(0f,numSpawned * 5.40f,0f);
+        }
+        
+        numSpawned++;
+        
+    }
+
+    void Update()
+    {
+        if(buildnumber > numSpawned)
+        {
+            
+            Spawn();
+        }
+    }
+}
