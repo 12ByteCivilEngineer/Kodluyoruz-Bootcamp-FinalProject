fbb205baaa61c1e5bb865772965ad980fe03ce24
Oğuz SEKMEN <78972141+Oguzsekmen@users.noreply.github.com>
2021-04-05 19:02:53 +0300

    Oguz

---
+++ Assets/Scripts/Player/vakum.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vakum : MonoBehaviour
+{
+    public GameObject leftHand;
+    
+    void Update()
+    {
+        gameObject.transform.position = leftHand.transform.position;
+    }
+}
---
+++ Assets/Scripts/Player/vakum1.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vakum1 : MonoBehaviour
+{
+    public GameObject rigthHand;
+    
+    void Update()
+    {
+        gameObject.transform.position = rigthHand.transform.position;
+    }
+}
