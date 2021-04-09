c39b7047f3ce09c6b05fa6c945a28646b7ee6c8b
Doğuhan <58668943+12ByteCivilEngineer@users.noreply.github.com>
2021-04-09 20:53:58 +0300

    asd

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody sousage;
+    public Rigidbody thief;
+    public Animator animForSouage;
+    public Animator animForThief;
+
+    [SerializeField]
+    ParticleSystem boom;
+
+    void Update()
+    {
+        if (animForSouage.GetCurrentAnimatorStateInfo(0).IsName("JumpOn"))
+        {
+            thief.transform.rotation = Quaternion.Euler(0, 80, 0);
+        }
+        if (animForSouage.GetCurrentAnimatorStateInfo(0).IsName("JumpOn"))
+        {
+            sousage.transform.rotation = Quaternion.Euler(0, -90, 0);
+            animForThief.Play("ReadyToFight");
+            boom.Play();
+        }
+
+        if (boom.IsAlive(true))
+        {
+            thief.useGravity = true;
+            thief.transform.position = Vector3.MoveTowards(thief.transform.position, thief.transform.position - new Vector3(0, 0, 4), 0.09f);
+            Invoke("ScaleCharacter", 2.0f);
+        }
+        
+    }
+
+    void ScaleCharacter()
+    {
+        sousage.transform.rotation = Quaternion.Euler(0, 180, 0);
+    }
+}
