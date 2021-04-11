e9cf0d0cf6a2165c5d55b57ece99077d77a63d6f
Ahmet Harun Tokyer <ahmetharuntokyer@gmail.com>
2021-04-11 23:04:51 +0300

    Level Design

---
+++ Assets/Scripts/Vase/Vazocu.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vazocu : MonoBehaviour
+{
+
+    Animator animator;
+    bool isRunning;
+
+    private void Start()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        isRunning = false;
+    }
+
+    private void Update()
+    {
+        
+        if (Pavement.isGameStarted && VaseSpawner.isSpawned)
+        {
+            isRunning = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+
+        if (isRunning == true)
+        {
+            animator.SetBool("Running", true);
+        }
+        if (isRunning == false)
+        {
+            animator.SetBool("Running", false);
+        }
+
+    }
+
+   
+
+
+
+
+    //private Animator animator;
+    //float timer =0;
+    //private void Start()
+    //{
+    //    animator = GetComponent<Animator>();
+    //    //animator.speed = 0;
+    //    //StartCoroutine(StartAnimation());
+    //    animator.GetComponent<Animator>().enabled = false;
+
+    //}
+
+    //void Repeat()
+    //{
+    //    StartCoroutine(StartAnimation());
+    //    animator.GetComponent<Animator>().enabled = false;
+    //}
+
+
+    //IEnumerator StartAnimation()
+    //{
+
+    //    yield return new WaitForSeconds(5f);
+    //    animator.GetComponent<Animator>().enabled = true;
+    //    yield return new WaitForSeconds(5f);
+    //    Repeat();
+    //    yield return null;
+    //}
+
+
+    //private void Update()
+    //{
+    //    timer += Time.deltaTime;
+    //    if (Pavement.isGameStarted)
+    //    {
+    //        Repeat();
+    //        //if (timer >=5 )
+    //        //{
+    //        //    animator.GetComponent<Animator>().enabled = true;
+
+    //        //    timer = -2;
+    //        //}
+    //        //if (timer ==0)
+    //        //{
+    //        //    animator.GetComponent<Animator>().enabled = false;
+
+    //        //}
+    //    }
+    //}
+
+}
