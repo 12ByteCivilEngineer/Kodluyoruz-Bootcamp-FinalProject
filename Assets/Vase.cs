8ff164f651e19a64220e1d5d8a64df977c3d1f95
dogukanuraz <dogukanuraz@outlook.com>
2021-04-12 16:11:44 +0300

    raycast

--- Assets/Vase.cs
+++ Assets/Vase.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Vase : MonoBehaviour
 {
     // Start is called before the first frame update
@@ -9,11 +9,32 @@ public class Vase : MonoBehaviour
     MeshRenderer meshrenderer;
     Rigidbody body;
     BoxCollider boxCollider;
+    GameObject attentionImage;
+
     void Awake()
     {
         meshrenderer = GetComponent<MeshRenderer>();
         body = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
+        attentionImage = GameObject.FindGameObjectWithTag("Attention");
+        //attentionImage.SetActive(false);
+    }
+
+    private void Update()
+    {
+        int layerMask = LayerMask.GetMask("Player");
+        RaycastHit raycastHit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out raycastHit, Mathf.Infinity, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.down) * raycastHit.distance, Color.red);
+            Debug.Log("Raycast çalışıyor");
+            attentionImage.SetActive(true);
+        }
+
+        else
+        {
+            attentionImage.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
